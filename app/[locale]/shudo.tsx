fbbd1e53b4ad9a2583e332de71541e64650eb779
g283858 <github@283858.xyz>
2024-12-10 03:06:24 +0800

    Update shudo.tsx

--- app/[locale]/shudo.tsx
+++ app/[locale]/shudo.tsx
@@ -1,12 +1,14 @@
 "use client"
-
 import React from 'react';
 import EmojiSudoku from '@/components/Games/shudo';
 
-export default function Page() { 
+const Index: React.FC = () => {
+  
   return (
     <div className="min-h-screen bg-purple-50">
       <EmojiSudoku />
     </div>
   );
-}
+};
+
+export default Index;
