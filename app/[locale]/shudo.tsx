df91ea70b651bcfd166f5836c213ec4797c8cbf1
g283858 <github@283858.xyz>
2024-12-10 02:39:00 +0800

    Create shudo.tsx

---
+++ app/[locale]/shudo.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react';
+import EmojiSudoku from '@/components/Games/shudo';
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-purple-50">
+      <EmojiSudoku />
+    </div>
+  );
+}
